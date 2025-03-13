1ca3a792a852962569008672800c25c7df0ec96a
tsots <mosalatsotetsi@gmail.com>
2025-03-14 01:42:28 +0200

    link to jobs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
       Home Hello to me
       <p>Flashy</p>
+      <Link to="jobs">View Jobs</Link>
     </div>
   );
 };
